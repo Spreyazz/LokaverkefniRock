dfd4ff7f059d2cf2a3f7827d6bc87e3bc236a12f
Spreyazz <spreyazz@hotmail.com>
2016-12-03 13:54:05 +0000

    panter promo

--- undirsida.php
+++ undirsida.php
@@ -3,6 +3,7 @@
  if($_GET["id"] == "rammstein")
  {
      $img = "./Myndir/Rammstein.png";
+     $title = "Rammstein";
      $LysingAhljomsveitinni = "Rammstein <br>
     meðlimar:<br>
     Frá vinstri <br>
@@ -19,6 +20,8 @@
  if($_GET["id"] == "pantera")
 {
      $img = "./Myndir/Pantera.png";
+     $title = "Pantera";
+     $img2 = "./Myndir/Panterapromo.png";
      $LysingAhljomsveitinni = "";
      $youtubelinkur = "https://www.youtube.com/embed/la1WfEr60yg";
      $bakgrunnur = "svartur";
@@ -26,6 +29,7 @@
  if($_GET["id"] == "metallica")
 {
      $img = "./Myndir/Metallica.png";
+     $title = "Metallica";
      $LysingAhljomsveitinni = "";
      $youtubelinkur = "https://www.youtube.com/embed/Tj75Arhq5ho";
      $bakgrunnur = "blar";
@@ -33,6 +37,7 @@
  if($_GET["id"] == "skillet")
  {
      $img = "./Myndir/Skillet.png";
+     $title = "Skillet";
      $LysingAhljomsveitinni = "";
      $youtubelinkur = "https://www.youtube.com/embed/uGcsIdGOuZY";
      $bakgrunnur = "grar";
@@ -40,6 +45,7 @@
 if($_GET["id"] == "DeepPurple")
 {
     $img = "./Myndir/Deep-Purple.png";
+    $title = "Deep Purple";
     $LysingAhljomsveitinni = "";
     $youtubelinkur = "https://www.youtube.com/embed/RKrNdxiBW3Y";
     $bakgrunnur = "grar";
@@ -47,6 +53,7 @@ if($_GET["id"] == "DeepPurple")
 if($_GET["id"] == "JimiHendrix")
 {
     $img = "./Myndir/JimiHendrix.png";
+    $title = "Jimi Hendrix";
     $LysingAhljomsveitinni = "";
     $youtubelinkur = "https://www.youtube.com/embed/TLV4_xaYynY";
     $bakgrunnur = "grar";
@@ -54,6 +61,7 @@ if($_GET["id"] == "JimiHendrix")
 if($_GET["id"] == "PinkFloyd")
 {
     $img = "./Myndir/Pink-Floyd.png";
+    $title = "Pink Floyd";
     $LysingAhljomsveitinni = "";
     $youtubelinkur = "https://www.youtube.com/embed/R49d4f5sEs4";
     $bakgrunnur = "grar";
@@ -61,6 +69,7 @@ if($_GET["id"] == "PinkFloyd")
 if($_GET["id"] == "TheWho")
 {
     $img = "./Myndir/the-who.png";
+    $title = "The Who";
     $LysingAhljomsveitinni = "";
     $youtubelinkur = "https://www.youtube.com/embed/5QaVzv5aR6U";
     $bakgrunnur = "grar";
@@ -68,6 +77,7 @@ if($_GET["id"] == "TheWho")
 if($_GET["id"] == "AZDC")
 {
     $img = "./Myndir/azdc.png";
+    $title = "AZ DC";
     $LysingAhljomsveitinni = "";
     $youtubelinkur = "https://www.youtube.com/embed/v2AC41dglnM";
     $bakgrunnur = "grar";
@@ -75,6 +85,7 @@ if($_GET["id"] == "AZDC")
 if($_GET["id"] == "Genesis")
 {
     $img = "./Myndir/genesis.png";
+    $title = "Genesis";
     $LysingAhljomsveitinni = "";
     $youtubelinkur = "https://www.youtube.com/embed/Ccs2rt0oSzQ";
     $bakgrunnur = "grar";
@@ -82,6 +93,7 @@ if($_GET["id"] == "Genesis")
 if($_GET["id"] == "RHCP")
 {
     $img = "./Myndir/rhcp.png";
+    $title = "Red Hot Chili Peppers";
     $LysingAhljomsveitinni = "";
     $youtubelinkur = "https://www.youtube.com/embed/Sb5aq5HcS1A";
     $bakgrunnur = "grar";
@@ -89,6 +101,7 @@ if($_GET["id"] == "RHCP")
 if($_GET["id"] == "Kiss")
 {
     $img = "./Myndir/Kiss.png";
+    $title = "Kiss";
     $LysingAhljomsveitinni = "";
     $youtubelinkur = "https://www.youtube.com/embed/iZq3i94mSsQ";
     $bakgrunnur = "grar";
@@ -96,6 +109,7 @@ if($_GET["id"] == "Kiss")
 if($_GET["id"] == "Milkey")
 {
     $img = "./Myndir/Milkey.png";
+    $title = "Milkey";
     $LysingAhljomsveitinni = "";
     $youtubelinkur = "https://www.youtube.com/embed/qlGQoxzdwP4";
     $bakgrunnur = "grar";
@@ -103,6 +117,7 @@ if($_GET["id"] == "Milkey")
 if($_GET["id"] == "SvavarKnutur")
 {
     $img = "./Myndir/svavar-Knutur.png";
+    $title = "Svavar Knútur";
     $LysingAhljomsveitinni = "";
     $youtubelinkur = "https://www.youtube.com/embed/fvmalKyJ6NM";
     $bakgrunnur = "grar";
@@ -113,7 +128,7 @@ if($_GET["id"] == "SvavarKnutur")
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title></title>
+  <title><?= ($title); ?></title>
   <!-- CSS  -->
     <link rel="stylesheet" type="text/css" href="css/Stilsida.css">
     <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
@@ -125,6 +140,8 @@ if($_GET["id"] == "SvavarKnutur")
         <div class="rammiundirsida1">
             <img src="<?= ($img); ?>">
         </div>
+    </div>
+    <div class="rammi2-2">
         <div class="rammiundirsida2 textiPlayfair">
             <?php
             echo $LysingAhljomsveitinni;
